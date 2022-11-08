07880f8111de6fa6454e380271f50e60ec5c04c1
Source Academy User <Tan Shi Yu>
2022-11-08 14:42:33 +0800

    Changes made from Source Academy

--- PA_1819_Practice.js
+++ PA_1819_Practice.js
@@ -1,3 +1,22 @@
+// Task 1A
 function make_big_int_from_number(num) {
-    num % 10
+    if (num === 0) {
+        return list(0);
+    }
+    
+    let output = null;
+    while (num !== 0) {
+        let remainder = num % 10;
+        if (is_null(output)) {
+            output = list(remainder);
+            num = (num - remainder)/10;
+            continue;
+        }
+        
+        output = append(output, list(remainder));
+        num = (num - remainder)/10;
+    }
+    return output;
 }
+
+// make_big_int_from_number(24234);
