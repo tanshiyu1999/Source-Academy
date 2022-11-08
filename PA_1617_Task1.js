fb7df536de7f7a9b95b8c34098802953251f38ec
Source Academy User <Tan Shi Yu>
2022-11-08 17:48:21 +0800

    Changes made from Source Academy

--- PA_1617_Task1.js
+++ PA_1617_Task1.js
@@ -1,6 +1,10 @@
 // Task 1A
+function is_nucleobase(str) {
+    if (str === "A" || str === "C" || str === "G" || str === "T") {
+        return true;
+    }
+    return false;
+}
 
-
-
-
+is_nucleobase("A");
 
